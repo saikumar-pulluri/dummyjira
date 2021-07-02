98155e5bf990811d26cb211ae4a1efc716df93a8
saikumar-pulluri <pullurisaikumar0001@gmail.com>
2021-07-02 20:41:49 +0530

    FIRST DUMMY COMMIT JIRA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BacklogComponent } from './backlog/backlog.component';
+import { IssuesComponent } from './issues/issues.component';
+import { SprintComponent } from './sprint/sprint.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"backlog",component:BacklogComponent},
+  {path:"issues",component:IssuesComponent},
+  {path:"sprint",component:SprintComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { IssuesComponent } from './issues/issues.component';
+import { SprintComponent } from './sprint/sprint.component';
+import { BacklogComponent } from './backlog/backlog.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopbarComponent,
+    IssuesComponent,
+    SprintComponent,
+    BacklogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    NgbModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
